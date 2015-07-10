1dddc6797f3976df0d31b1bd333c010060ec6b0c
perlancar (@pc-office) <perlancar@gmail.com>
2015-07-10 19:35:36 +0700

    Release v0.01

---
+++ lib/App/IndonesianLocaleUtils.pm
@@ -0,0 +1,18 @@
+package App::IndonesianLocaleUtils;
+
+use 5.010001;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLIs related to Indonesian locale
+
+=head1
+
+This distribution contains several CLI's related to Indonesian locale:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
